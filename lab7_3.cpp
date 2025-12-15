978f737bb729814e185caae32319b31aa447514a
pubkung1ez <tirapan_30506@sysp.ac.th>
2025-12-15 16:20:28 +0700

    lab7 suss

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    if (x < 'A' or x > 'Z') {
+        return '0';
+    }
+    else if (x == 'A') {
+        return 'Z';
+    }
+    else {
+        return x - 1;
+    }
 }
 
 int main(){
